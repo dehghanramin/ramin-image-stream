048fe9210447e389785243ffd00a4c01808e122f
Ramin Dehghan <ramin@wolog.org>
2023-10-10 08:07:15 -0700

    Add video stream capacity

--- server.py
+++ server.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, Response
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import cv2
 
 # Define a counter metric
 counter_metric = Counter('microsample_calls_total', 'Total calls to microsampl api (counter)')
 
 __info = {
-    'name': 'Erik Lonroth',
-    'skill': 100,
-    'title': 'wizard'
+    'name': 'Ramin Dehghan',
+    'email': 'online@wolog.org',
+    'title': 'dev'
 }
 
 info = __info
+
+# Define stream capture
+def get_frame():
+    camera=cv2.VideoCapture(0)
+
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 application = Flask(__name__)
 
 @application.route('/')
 def index():
     counter_metric.inc()
-    return "Online"
+    return "Functional"
+
 
 # Show info
 @application.route('/api/info', methods=['GET'])
@@ -26,6 +43,7 @@ def get_info():
     # Increase counter to 
     counter_metric.inc()
     return jsonify(info)
+
 
 # Provide dummy metrics for prometheus
 @application.route('/metrics')
@@ -35,11 +53,17 @@ def metrics():
     prometheus_metrics = generate_latest()
 
     return Response(prometheus_metrics, mimetype=CONTENT_TYPE_LATEST)
-    
+
+
+# Stream video
+@application.route('/video')
+def video():
+     return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 def main():
     application.run()
+
 
 if __name__ == '__main__':
     application.run()
