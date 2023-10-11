55985b9e93078362de8490735ccba393ce2d56a6
Ramin Dehghan <ramin@wolog.org>
2023-10-10 21:34:26 -0700

    Update camera stream logic

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, Response
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
+from PIL import Image
 import cv2
 
 # Define a counter metric
@@ -16,15 +20,22 @@ info = __info
 
 # Define stream capture
 def get_frame():
-    camera=cv2.VideoCapture(0)
-    success, frame = camera.read()
-    if not success:
-        return "No Image"
-    ret, buffer = cv2.imencode('.jpg', frame)
-    frame = buffer.tobytes()
-    camera.release()
+    # camera=cv2.VideoCapture(0)
+    # success, frame = camera.read()
+    # if not success:
+    #     yield("No Image")
+    # ret, buffer = cv2.imencode('.jpg', frame)
+    # rame = buffer.tobytes()
+    # camera.release()
+    stream = BytesIO()
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(2)
+    camera.capture(stream, 'jpeg')
+    stream.seek(0)
+    image = Image.open(stream)
     yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+           b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
 
 
 application = Flask(__name__)
